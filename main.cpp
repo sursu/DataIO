9e0d948a5def4f917da26cf1b388f9f0d9b1c006
Sandu Ursu <sandu.ursu@gmail.com>
2018-05-26 20:34:46 +0200

    Reading and Writing added. Updated .gitignore

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <string>
+
+#include <cassert>
+#include <fstream>
 
 #include "ReadFile.hpp"
+#include "ReadFile.cpp"
+#include "Exception.hpp"
+#include "Exception.cpp"
 
 using namespace std;
 
@@ -11,11 +18,108 @@ int main()
     // Start clock
 	auto start = std::chrono::high_resolution_clock::now();
 
-    cout << "Let's get started!!" << endl;
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	string filename = "/home/sandu/bmetric/C++ Projects/NoSessions/sessions.csv";
+
+	vector<vector<long int>> Table; // future destination Table
+
+
+	// Read data from filename to Table in an exception safely manner ////////////////////////////
+	try {
+		ReadFile(filename, Table);
+	}
+	catch (Exception& error) {
+		error.PrintDebug();
+		std::cout << "Couldn't open the file\n";
+		std::cout << "Give alternative location\n";
+		std::cin >> filename;
+		ReadFile(filename, Table);
+	}
+
+	cout << "Number of rows: " << Table.size() << "\n\nFirst few rows:\n";
+	for (auto i = 0; i < 10; ++i) {
+		for (auto el : Table[i]) 
+			cout << el << '\t';
+		cout << endl;
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Iterate over visitors and check how many unique sessions have they had in the past 30 days (2592000s)
+	for (auto i = 0; i < Table.size(); ++i) {
+		unsigned n = 1;
+		long int last = Table[i][2];
+		for (auto j = i; (Table[j][0] == Table[i][0]) && (Table[j][1] > Table[i][1] - 2592000) && (j > 0); --j) {
+			long int session = Table[j][2];
+			if (session != last) {
+				n += 1;
+				last = session;
+			}
+		}
+		Table[i].push_back(n);
+	} 
+
+
+	cout << "\n\nFirst 100 rows with counted sessions\n" << endl;
+
+	for (auto i = 0; i < 100; ++i) {
+		for (auto el : Table[i])
+			cout << el << '\t';
+		cout << endl;
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	//// Write the table to file .csv
+	//cout << "Writing the table to file...\n";
+
+	//ofstream ofile("sessions_numbered.csv");
+	////assert(ofile.is_open());
+
+	//size_t nCols = Table[0].size();
+
+	//// write numeric bitmap
+	//for (int n = 0; n < Table.size(); ++n) {
+	//	for (auto i = 0; i < nCols-1; ++i) {
+	//		ofile << Table[n][i] << ",";
+	//	}
+	//	ofile << Table[n][nCols-1] << "\n";
+	//}
+
+	//ofile.flush();
+	//ofile.close();
+
+	//------------------------------------------------------------------------------------------------
+
+	// Write the table to file .csv (BINARY)
+	cout << "Writing the table to file..." << endl;
+
+	ofstream ofile("/home/sandu/bmetric/C++ Projects/DataIO/sessions_numbered.csv", std::ios::binary);
+	assert(ofile.is_open());
+
+	size_t nCols = Table[0].size();
+
+	// write numeric bitmap
+	for (int n = 0; n < Table.size(); ++n) {
+		for (auto i = 0; i < nCols - 1; ++i) {
+			ofile.write(reinterpret_cast<char*>(&Table[n][i]), sizeof(long int));
+		}
+		ofile.write(reinterpret_cast<char*>(&Table[n][nCols - 1]), sizeof(long int));
+	}
+
+	ofile.flush();
+	ofile.close();
+
+    cout << "Operation completed successfully!\n" << endl;
+	//------------------------------------------------------------------------------------------------
 
     // Stop clock 
 	auto end = std::chrono::high_resolution_clock::now();
 	cout << "Computation time: " << std::chrono::duration<double, milli>(end - start).count() << "ms\n";
-
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    cout << endl;
     return 0;
 }
