e13dbe322d04ed8148ad2c92035941331b2dea81
Sandu Ursu <sandu.ursu@gmail.com>
2018-05-29 17:41:06 +0200

    Changes in iodata.hpp

--- iodata.hpp
+++ iodata.hpp
@@ -1,19 +1,34 @@
 #ifndef DATAIODEF
 #define DATAIODEF
 
+#include <string>
+
 // template 
 class File
 {
 private:
     std::string mData;
-    int 
+    int nRows;
 public:
     File(const std::string& fileName);
     void PrintDebug() const;
 
     // make a decorator for it to be verbose
 
-    void shape();
-    void head(auto i);
+    void dim();
+    void len();
+
+    void head(size_t i);
+    void tail(size_t i);
+
+    void nth(size_t i);
+    void last(size_t i);
+
+    // overload selection operator
+    // select column
+    // specify categories (enum)
+
+    
+
 };
 #endif //DATAIODEF
